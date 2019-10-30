bb3f6a8ad51bff06e579e02ecc1e8a8322e7a807
i3Cheese <i3Cheese@yandex.ru>
2019-10-30 12:12:54 +0700

    Add SetupDB in main.py

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from ui_mainWindow import Ui_MainWindow
 from dialogs import CategoryCreateDialog
+import sqlite3
+
+DATA_BASE = '_Your_Diary.db'
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.setupDB()
+
+    def setupUi(self, MainWindow):
+        super().setupUi(MainWindow)
         self.acCreate.triggered.connect(self.create_category)
 
     def create_category(self) -> None:
@@ -20,6 +27,56 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def addCategory(self, title, category) -> None:
         pass
+
+    def setupDB(self):
+        """Подключается к существующей или создаёт свою базу данных.
+        Если в папке с программой существовала база с таким же названием, но другой архитектурой
+        программа будет работать некорректно."""
+        if sys.path.exists(data_base):
+            return None
+        else:
+            con = sqlite3.connect(data_base)
+            cur = con.cursor()
+
+            # Создаём таблицу типов
+            cur.execute('''CREATE TABLE types ('''
+                        '''id   INTEGER PRIMARY KEY AUTOINCREMENT,'''
+                        '''name STRING  UNIQUE);''')
+            cur.execute('''INSERT INTO types(name) VALUES('default'); '''
+                        '''INSERT INTO types(name) VALUES('budget');''')
+
+            # Создаём таблицу категорий
+            cur.execute('''CREATE TABLE categories (
+    id       INTEGER  PRIMARY KEY ON CONFLICT REPLACE AUTOINCREMENT
+                      NOT NULL,
+    category INTEGER  REFERENCES types (id) ON DELETE CASCADE,
+    title    STRING,
+    creation DATETIME);''')
+
+            # Создаём таблицу флагов
+            cur.execute('''CREATE TABLE flags (
+    id       INTEGER PRIMARY KEY ON CONFLICT REPLACE AUTOINCREMENT
+                     NOT NULL,
+    category INTEGER REFERENCES categories (id) ON DELETE CASCADE
+                     NOT NULL,
+    title    STRING,
+    red      INTEGER,
+    green    INTEGER,
+    blue     INTEGER);''')  # red, green, blue - значение каналов RGB
+
+            # Создаём таблицу записей стандартного типа
+            cur.execute('''CREATE TABLE defaultNotes (
+    id       INTEGER  PRIMARY KEY ON CONFLICT REPLACE AUTOINCREMENT,
+    category INTEGER  REFERENCES categories (id) ON DELETE CASCADE,
+    text     TEXT,
+    flag     INTEGER  REFERENCES flags (id) ON DELETE SET NULL,
+    start    DATETIME,
+    [end]    DATETIME);''')
+
+            # TODO: Создаём таблицу записей типа budget
+
+            con.commit()
+            con.close()
 
 
 if __name__ == '__main__':
